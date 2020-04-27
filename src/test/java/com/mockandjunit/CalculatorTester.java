2ae2cfba3bd9b65e11c8af786fb691726cdea4ec
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-04-27 12:19:03 +0100

    Added special check on the number of calls that can be made on a particular method!

--- src/test/java/com/mockandjunit/CalculatorTester.java
+++ src/test/java/com/mockandjunit/CalculatorTester.java
@@ -1,9 +1,9 @@
 package com.mockandjunit;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.assertEquals;
 // Verify ensures whether a mock method is being called with the required arguments or not 
-import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.verify;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -37,7 +37,7 @@ public class CalculatorTester {
 //		verify(calcService).add(10.0, 20.0); 
 		
 		// Verify with different arguments: 
-		verify(calcService.add(20.0, 30.0));
+//		verify(calcService.add(20.0, 30.0));
 		/* This will cause an error:
 		 * testAdd(com.mockandjunit.CalculatorTester): 
 		 * Argument passed to verify() is of type Double and is not a mock!
@@ -48,6 +48,33 @@ public class CalculatorTester {
 		 * verify(mock, atLeastOnce()).someMethod();
 		 * false
 		 */
+		
+		// __________________________________________________________
+		
+		/* Mockito provides a special check on the number of calls that can be made on a particular method. 
+		 * Suppose Calculator should call the CalculatorService.serviceUsed() method only once, 
+		 * then it should not be able to call CalculatorService.serviceUsed() more than once.
+		 */
+		
+		// add the behaviour of calc service to subtract two numbers:
+		when(calcService.sub(20.0,10.0)).thenReturn(10.0);
+		
+		// test the add functionality
+		assertEquals(calc.add(10.0, 20.0),30.0,0);
+		assertEquals(calc.add(10.0, 20.0),30.0,0);
+		assertEquals(calc.add(10.0, 20.0),30.0,0);
+		
+		// test the subtract functionality 
+		assertEquals(calc.sub(20.0, 10.0),10.0,0);
+		
+		// default call count = 1
+		verify(calcService).sub(20.0,10.0); 
+		
+		// check if add function is called 4 times
+		verify(calcService, times(4)).add(10.0,20.0);
+		
+		// verify that method multiply was never called on a mock
+		verify(calcService, never()).mult(10.0,20.0);
 	}
 	
 }
