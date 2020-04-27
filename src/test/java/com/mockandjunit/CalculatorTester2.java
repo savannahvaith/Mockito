bbbd258045e8819e11433ab94302bf08ab20e04e
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-04-27 16:46:48 +0100

    Spy Object Added

--- src/test/java/com/mockandjunit/CalculatorTester2.java
+++ src/test/java/com/mockandjunit/CalculatorTester2.java
@@ -6,21 +6,25 @@ import static org.mockito.Mockito.*;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.InOrder;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CalculatorTester2 {
 
-	private Calculator calc; 
-	private CalculatorService cs; 
-	
+	private Calculator calc;
+	private CalculatorService cs;
+
 	@Before
 	public void setUp() {
-		calc = new Calculator(); 
+		calc = new Calculator();
 		cs = mock(CalculatorService.class);
+		MathApplication mapp = new MathApplication(); 
+		cs = spy(mapp);
 		calc.setCalculatorService(cs);
 	}
-	
+
 //	@Test
 //	public void testAddandSub() {
 //		when(cs.add(20.0,10.0)).thenReturn(30.0);
@@ -33,34 +37,95 @@ public class CalculatorTester2 {
 //		verify(cs).sub(20.0,10.0);
 //		
 //	}
+
+	/*
+	 * -----------------------------------------------------------------------------
+	 * ------------------- Mockito provides inorder class which takes care of the
+	 * order of method calls that the mock is going to make in due course of its
+	 * action:
+	 */
+
+//	@Test
+//	public void testAddAndSub() {
+//		when(cs.add(20.0,10.0)).thenReturn(30.0);
+//		when(cs.sub(20.0,10.0)).thenReturn(10.0);
+//		
+//		assertEquals(calc.add(20.0,10.0),30.0,0);
+//		assertEquals(calc.sub(20.0,10.0),10.0,0);
+//		
+//		verify(cs).add(20.0,10.0);
+//		verify(cs).sub(20.0,10.0);
+//		
+////		create an inOrder verifier for a single mock 
+//		InOrder inOrder = inOrder(cs);
+//		inOrder.verify(cs).sub(20.0, 10.0);
+//		inOrder.verify(cs).add(20.0,10.0);
+//		
+//		/* 
+//		 * This will throw an error because we are expecting the add feature FIRST 
+//		 * Verification in order failure
+//		 * Wanted but not invoked: calculatorService.add(20.0, 10.0);
+//		 * 
+//		 */
+//	}
+
+	/*
+	 * -----------------------------------------------------------------------------
+	 * ------------------- Mockito provides a Answer interface which allows stubbing
+	 * with generic interface.
+	 */
+
+//	@Test
+//	public void testAdd() {
+//		when(cs.add(20.0, 10.0)).thenAnswer(new Answer<Double>() {
+//
+//			public Double answer(InvocationOnMock invocation) throws Throwable {
+//				// TODO Auto-generated method stub
+//				Object[] args = invocation.getArguments();
+//
+//				// get the mock
+//				Object mock = invocation.getMock();
+//
+//				// return the result
+//				return 30.0;
+//			}
+//		});
+//		
+//		assertEquals(calc.add(20.0,10.0),30.0,0);
+//	}
 	
 	
-	/* ------------------------------------------------------------------------------------------------
-	 * Mockito provides inorder class which takes care of the order of method calls that the mock is going
-	 * to make in due course of its action:
+	/* -----------------------------------------------------------------------------
+	 * -------------------
+	 * Mockito provides option to create spy on real objects. 
+	 * When spy is called, then actual method of real object is called.
 	 */
 	
 	@Test
-	public void testAddAndSub() {
-		when(cs.add(20.0,10.0)).thenReturn(30.0);
-		when(cs.sub(20.0,10.0)).thenReturn(10.0);
+	public void testAdd() {
+		// Perform operation on real obj
+		// Test the add functionality 
+		assertEquals(calc.add(20.0, 10.0),30.0,0);
+	}
+	
+	class MathApplication implements CalculatorService{
+
+		public double add(double arg1, double arg2) {
+			return arg1 + arg2; 
+			
+		}
+
+		public double sub(double arg1, double arg2) {
+			throw new UnsupportedOperationException("Method not implemented yet!");
+		}
+
+		public double div(double arg1, double arg2) {
+			throw new UnsupportedOperationException("Method not implemented yet!");
+		}
+
+		public double mult(double arg1, double arg2) {
+			throw new UnsupportedOperationException("Method not implemented yet!");
+		}
 		
-		assertEquals(calc.add(20.0,10.0),30.0,0);
-		assertEquals(calc.sub(20.0,10.0),10.0,0);
-		
-		verify(cs).add(20.0,10.0);
-		verify(cs).sub(20.0,10.0);
-		
-//		create an inOrder verifier for a single mock 
-		InOrder inOrder = inOrder(cs);
-		inOrder.verify(cs).sub(20.0, 10.0);
-		inOrder.verify(cs).add(20.0,10.0);
-		
-		/* 
-		 * This will throw an error because we are expecting the add feature FIRST 
-		 * Verification in order failure
-		 * Wanted but not invoked: calculatorService.add(20.0, 10.0);
-		 * 
-		 */
 	}
 }
